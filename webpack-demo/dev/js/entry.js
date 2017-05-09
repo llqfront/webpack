7fbff5b77c2a5b75833a12eef23b6ca231135a6b
xhanglutest1 <260137290@qq.com>
2017-05-09 13:28:36 +0800

    up

---
+++ webpack-demo/dev/js/entry.js
@@ -0,0 +1,5 @@
+document.write('新航路师徒学院');
+var app = require('./module.js');
+require('../css/index.scss')
+app.init();
+console.log(1)
---
+++ webpack-demo/dev/js/module.js
@@ -0,0 +1,10 @@
+define(function(require, exports, module) {
+  	var P = {
+		_init:function(){
+			document.write('这个好像1好熟悉哟');
+		}
+	}
+	module.exports = {
+		init:P._init
+	}
+})
